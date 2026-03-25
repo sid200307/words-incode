190b57a44d20a662fcf53e465ed8aa3e10fd88ae
Siddharth Doshi <107400138+sid200307@users.noreply.github.com>
2026-03-25 08:03:31 +0530

    Refactor main function for region URL processing

--- main.py
+++ main.py
@@ -22,52 +22,56 @@ def main():
   
     # insert_into_db(cursor, con, country_urls,countries_table)
     
-    # create_table(cursor,regions_urls_table,regions_urls_table)
-    # with ThreadPoolExecutor(max_workers=15) as executor:
-    #     future = executor.submit(regions_urls)
-    #     reg_urls = future.result()
-    # insert_into_db(cursor,con,reg_urls,regions_urls_table)
+    create_table(cursor,countries_table,regions_urls_table)
+    with ThreadPoolExecutor(max_workers=15) as executor:
+      future = executor.submit( regions_urls,countries_table,"Country","Url", "Region" )
 
+    reg_urls = future.result()
+    insert_into_db(cursor,con,reg_urls,regions_urls_table)
+
+   
     #sub regions
 
-    create_table(cursor,regions_urls_table,regions_urls_table)
+    
     # with ThreadPoolExecutor(max_workers=15) as executor:
-    #     future = executor.submit(sub_regions_urls)
+    #     future = executor.submit( regions_urls,regions_urls_table,"Country","Url", "Region" )
     #     sub_reg_urls = future.result()
     # insert_into_db(cursor,con,sub_reg_urls,sub_regions_table)
+
 
     #Sub SUb Regions
     
     # with ThreadPoolExecutor(max_workers=20) as executor:
-    #     future = executor.submit(sub_sub_regions_urls)
+    #     future = executor.submit( regions_urls,sub_regions_table,"Country","Url", "Region" )
     #     sub_reg_urls = future.result()
     # insert_into_db(cursor,con,sub_reg_urls,sub_sub_regions_table)
 
-    all_data = []
-    urls = fetch_urls(sub_sub_regions_table, "Sub_Sub_Region", "Url")
+# postcodes
+    # all_data = []
+    # urls = fetch_urls(sub_sub_regions_table, "Sub_Sub_Region", "Url")
 
-    with ThreadPoolExecutor(max_workers=80) as tpe:  # 50 threads for 42k urls
-        futures = {tpe.submit(parser, url): (region, url)
-                   for region, url in urls}
-        for future in as_completed(futures):
-            region, url = futures[future]
-            try:
-                result = future.result()
-                if result:
-                    all_data.extend(result)
-                    update_q(sub_sub_regions_table, "Url", url)
-            except Exception as e:
-                print(f"Error {url}: {e}")
+    # with ThreadPoolExecutor(max_workers=80) as tpe:  # 50 threads for 42k urls
+    #     futures = {tpe.submit(parser, url): (region, url)
+    #                for region, url in urls}
+    #     for future in as_completed(futures):
+    #         region, url = futures[future]
+    #         try:
+    #             result = future.result()
+    #             if result:
+    #                 all_data.extend(result)
+    #                 update_q(sub_sub_regions_table, "Url", url)
+    #         except Exception as e:
+    #             print(f"Error {url}: {e}")
 
-        # insert in batches as results come in
-        if len(all_data) >= 100:
-            insert_into_db(cursor, con, all_data, pincodes_tab)
-            all_data.clear()
+    #     # insert in batches as results come in
+    #     if len(all_data) >= 100:
+    #         insert_into_db(cursor, con, all_data, pincodes_tab)
+    #         all_data.clear()
 
-    # insert remaining
-    if all_data:
-        insert_into_db(cursor, con, all_data, pincodes_tab)
-    #commiting db changes
+    # # insert remaining
+    # if all_data:
+    #     insert_into_db(cursor, con, all_data, pincodes_tab)
+    # #commiting db changes
     con.commit()
 
     #closing db connetions
